04bc54791ef36e7714048fbf7969803862308967
ajlawrence <aj.lawrence@thejagroup.com>
2015-01-28 21:06:42 +0000

    These are a bunch of changes

--- app/models/notes2.rb
+++ app/models/notes2.rb
@@ -1,12 +1,9 @@
 class Note < ActiveRecord::Base
- belongs_to :user
+  belongs_to :user
 
   validates :title, presence: true
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
-  def preview
-    self.content
-    self.content.slice(0..100) << "...(readmore)"
-  end
+
 end
